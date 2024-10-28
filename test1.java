594bf0ec3d56d6acede241ab8ce6356a131ad6c9
Ranjit <ranjit9348sahoo@gmail.com>
2024-10-28 15:21:49 +0530

    all thing complete

--- test1.java
+++ test1.java
@@ -8,11 +8,8 @@ public class test1{
 
     public void m2(){
         System.out.println("Sujit Sahoo");
-<<<<<<< Updated upstream
         System.out.println("HaraPriya Sahoo");
         System.out.println("Shreyansh Sekhar Sahoo");
-=======
->>>>>>> Stashed changes
     }
    
     public void m4(){
