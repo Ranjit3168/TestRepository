fd6ff4c6177bb6b65644eb67dcb26afbbb993010
Ranjit <ranjit9348sahoo@gmail.com>
2024-10-28 15:20:05 +0530

    pending task complete

--- test1.java
+++ test1.java
@@ -5,10 +5,18 @@ public class test1{
 
      public void m3(){
         System.out.println("Suvasmita Sahoo");
-        
+
     public void m2(){
         System.out.println("Sujit Sahoo");
+<<<<<<< Updated upstream
         System.out.println("HaraPriya Sahoo");
         System.out.println("Shreyansh Sekhar Sahoo");
+=======
+>>>>>>> Stashed changes
     }
+   
+    public void m4(){
+        System.out.println("Working going on....");
+    }
+    
 }
