2e81d09baf0c87d0c517fb3a5f1eff15e78d1371
Ranjit <ranjit9348sahoo@gmail.com>
2024-10-28 15:18:01 +0530

    complete task for method m2()

--- test1.java
+++ test1.java
@@ -8,6 +8,7 @@ public class test1{
         
     public void m2(){
         System.out.println("Sujit Sahoo");
-
+        System.out.println("HaraPriya Sahoo");
+        System.out.println("Shreyansh Sekhar Sahoo");
     }
 }
